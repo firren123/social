34415c6d1c5152ed56897986483f404b3f2901a4
林新亮 <linxinliang@iyangpin.com>
2015-08-06 15:07:34 +0800

    新增首页

---
+++ frontend/web/index.php
@@ -0,0 +1,12 @@
+<?php
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../common/config/bootstrap.php');
+$config = require(__DIR__ . '/../../common/config/main.php');
+//error_reporting(E_ALL^E_NOTICE); //屏蔽NOTICE
+
+$application = new yii\web\Application($config);
+$application->run();
