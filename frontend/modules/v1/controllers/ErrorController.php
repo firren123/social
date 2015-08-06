82e2fffaec131422b90323d37cece8cfb1d70557
林新亮 <linxinliang@linxinlangdeAir.lan>
2015-08-06 11:11:03 +0800

    新增v1版本

---
+++ frontend/modules/v1/controllers/ErrorController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 错误提示
+ *
+ * PHP Version 5
+ *
+ * @category  Social
+ * @package   ERROR
+ * @author    linxinliang <linxinliang@iyangpin.com>
+ * @time      2015/8/06
+ * @copyright 2015 灵韬致胜（北京）科技发展有限公司
+ * @license   http://www.i500m.com license
+ * @link      linxinliang@iyangpin.com
+ */
+
+namespace frontend\modules\v1\controllers;
+
+use yii\web\Controller;
+/**
+ * ERROR
+ *
+ * @category Social
+ * @package  ERROR
+ * @author   linxinliang <linxinliang@iyangpin.com>
+ * @license  http://www.i500m.com/ license
+ * @link     linxinliang@iyangpin.com
+ */
+class ErrorController extends Controller
+{
+    /**
+     * 错误页面提示
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $arr = array(
+            'code' => '404',
+            'data' => [],
+            'message' => '抱歉，请求方法不存在',
+        );
+        echo json_encode($arr);
+    }
+}
---
+++ frontend/modules/v1/views/plug/sign.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+        <title>Sign签名小工具</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylesheet" href="/plug/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/plug/bootstrap/css/bootstrap-theme.min.css">
+        <script type="text/javascript" src="/js/jquery-2.0.3.js"></script>
+        <script src="/plug/bootstrap/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <div class="container">
+        <h2>Sign签名小工具</h2>
+
+        <!-- appID -->
+        <div class="input-group" style="margin-top: 5px;">
+            <span class="input-group-addon">appID</span>
+            <input type="text" class="form-control" readonly value="I500_SOCIAL">
+        </div>
+
+        <!-- app_code -->
+        <div class="input-group" style="margin-top: 5px;">
+            <span class="input-group-addon">appCode</span>
+            <input type="text" class="form-control" readonly value="DKJA@(SL)RssMAKDKas!L">
+        </div>
+
+        <!-- timestamp -->
+        <div class="input-group" style="margin-top: 5px;">
+            <span class="input-group-addon">timestamp</span>
+            <input type="text" class="form-control" readonly value="<?php echo $data['timestamp'];?>">
+        </div>
+
+
+        <?php if(!empty($data['msg'])) {
+            ?>
+            <div style="margin-top: 10px; text-align: center;" class="alert alert-success" role="alert">
+                <?php echo $data['msg'];?>
+            </div>
+            <?php
+        }?>
+    </div>
+    </body>
+</html>
+
