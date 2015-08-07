0f7c3c9d13ce1450e235439577ddaa0a63472ddb
lichenjun <firren@163.com>
2015-08-07 11:13:07 +0800

    文件

---
+++ frontend/modules/sms/Module.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * 简介1
+ *
+ * PHP Version 5
+ *
+ * @category  PHP
+ * @package   Social
+ * @filename  Module.php
+ * @author    lichenjun <lichenjun@iyangpin.com>
+ * @copyright 2015 www.i500m.com
+ * @license   http://www.i500m.com/ i500m license
+ * @datetime  15/8/7 上午10:40
+ * @version   SVN: 1.0
+ * @link      http://www.i500m.com/
+ */
+namespace frontend\modules\sms;
+/**
+ * Class Module
+ * @category  PHP
+ * @package   Module.php
+ * @filename  Module.php
+ * @author    lichenjun <lichenjun@iyangpin.com>
+ * @copyright 2015 www
+ * @license   http://www.i500m.com/ i500m license
+ * @datetime  ${DATE} ${TIME}
+ * @link      http://www.i500m.com/
+ */
+class Module extends \Yii\Base\Module
+{
+    /**
+     * 初始化方法
+     * @return string
+     */
+    public function init()
+    {
+        parent::init();
+    }
+
+}
