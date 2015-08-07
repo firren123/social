94584102ce8ae9b81b3dfb5e5a30cc6847a87b3c
lichenjun <firren@163.com>
2015-08-07 11:23:56 +0800

    添加SMS通道

--- frontend/modules/channel/controllers/IndexController.php
+++ frontend/modules/channel/controllers/IndexController.php
@@ -40,4 +40,51 @@ class IndexController extends Controller
     {
         echo "这是通道模块，请完善。";
     }
+
+    /**
+     * 简介：
+     * @author  lichenjun@iyangpin.com。
+     * @throws Exception
+     * @return string
+     */
+    public function actionMq()
+    {
+        $config = array(
+            'host' => '118.186.247.55',
+            'port' => 5672,
+            'vhost' => '500m',
+            'login' => '500m',
+            'password' => 'gbjY51Rpstx'
+        );
+        $conn = new AMQPConnection($config);
+
+        if ($conn->connect()) {
+            echo "Established a connection to the broker <br />";
+        } else {
+            echo "Cannot connect to the broker <br />";
+        }
+
+        $conn->setHost('www.w3hacker.com');
+        var_dump($conn->getHost());
+        $conn->setLogin('admin');
+        var_dump($conn->getLogin());
+        $conn->setPassword('www.w3hacker.com');
+        var_dump($conn->getPassword());
+        $conn->setPort('1234');
+        var_dump($conn->getPort());
+        $conn->setTimeout(3000);
+        var_dump($conn->getTimeout());
+        $conn->setVhost('admin');
+        var_dump($conn->getVhost());
+        var_dump($conn->isConnected());
+
+        if (!$conn->disconnect()) {
+            throw new Exception('Could not disconnect');
+        } else {
+            var_dump("disconnect");
+        }
+
+        //$conn->reconnect();
+        var_dump($conn->isConnected());
+    }
 }
