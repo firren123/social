2cecfac7a474d8d59f36b487facbcff852157ed6
林新亮 <linxinliang@linxinliangdeMacBook-Air.local>
2015-08-10 11:26:17 +0800

    打开验证签名

--- common/config/params.php
+++ common/config/params.php
@@ -15,5 +15,5 @@ return [
     'mobilePreg'      => '/^1[34587][0-9]{9}$/',      //Mobile 验证规则
     'token_timeout'   => 7*24*60*60,                  //用户登陆token有效期
     'verify_code_timeout'   => 60*60,            //用户短信验证码有效期
-    'sign_debug'      => true,                       //false = 开启验证 true 关闭验证
+    'sign_debug'      => false,                       //false = 开启验证 true 关闭验证
 ];
