b91975af2b839f4010f0ce4d44c9941f9d2bde86
林新亮 <linxinliang@linxinlangdeAir.lan>
2015-09-06 11:51:11 +0800

    测试环境 开启短信通道

--- common/config/params.php
+++ common/config/params.php
@@ -16,7 +16,7 @@ return [
     'imgHost'         => 'http://img.test.i500m.com/', //图片服务器
     'channelHost'     => 'http://channel.test.i500m.com/', //通道服务器
     'saveSms'         => true, //true = 保存短信内容入库 false = 不保存短信入库
-    'openSmsChannel'  => false, //true = 开启短信通道 false = 关闭短信通道
+    'openSmsChannel'  => true, //true = 开启短信通道 false = 关闭短信通道
     'mobilePreg'      => '/^1[34587][0-9]{9}$/',       //Mobile 验证规则
     'token_timeout'   => 7*24*60*60,                   //用户登陆token有效期
     'verify_code_timeout'   => 60*60,                  //用户短信验证码有效期
