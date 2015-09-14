a3f33563db187cd63ce9538658bc92afc208e81d
林新亮 <linxinliang@iyangpin.com>
2015-09-14 15:54:26 +0800

    测试环境关闭ssdb

--- common/config/params.php
+++ common/config/params.php
@@ -5,7 +5,7 @@ return [
     'APP_CODE' => [
         'I500_SOCIAL' => 'DKJA@(SL)RssMAKDKas!L',
     ],
-    'openSSDB'        => true, //true = 开启 false = 关闭
+    'openSSDB'        => false, //true = 开启 false = 关闭
     'SSDBCacheTime'   => 60, //SSDB 缓存时间 60秒
     'openLog'         => true, //true = 开始日志 false = 关闭日志
     'returnLogFile'   => '/tmp/return_log.txt', //返回值日志文件
