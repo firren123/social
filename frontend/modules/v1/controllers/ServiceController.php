b42f5cc56895163cff5b17f8c0ceb9abb9ed2d5b
林新亮 <linxinliang@iyangpin.com>
2015-09-22 14:55:42 +0800

    新增服务描述字段

--- frontend/modules/v1/controllers/ServiceController.php
+++ frontend/modules/v1/controllers/ServiceController.php
@@ -554,7 +554,7 @@ class ServiceController extends BaseController
         }
         $service_model = new Service();
         $where['is_deleted']   = '2';
-        $fields = 'id,mobile,image,title,price,unit,service_way,audit_status,status';
+        $fields = 'id,mobile,image,title,description,price,unit,service_way,audit_status,status';
         $list = $service_model->getPageList($where, $fields, 'id desc', $page, $page_size);
         if (empty($list)) {
             $this->returnJsonMsg('1009', [], Common::C('code', '1009'));
